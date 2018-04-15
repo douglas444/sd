7399235b68dd29bd57a2a563e1baff3f1bfa2bc5
Tiago Faria <tiago_pereira_faria@hotmail.com>
2018-04-15 00:00:18 -0300

    server done

--- projeto1/server.py
+++ projeto1/server.py
@@ -4,6 +4,9 @@ import _thread;
 import queue;
 
 received_commands = multiprocessing.Queue();
+processing_commands = multiprocessing.Queue();
+
+dados = {}
 
 def config_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
@@ -12,24 +15,71 @@ def config_server():
     s.bind((host,port));
     return s
 
-def store_and_process():
+def store_and_log():
     while True:
         if (received_commands.qsize() > 0):
             command = received_commands.get();
-            store(command);
-        stored_command = get_stored_command();
+            print(str(command[0][0]) + ' ' + str(command[0][1]) + ' ' + command[1]);
+            store(str(command[0][0]) + ' ' + str(command[0][1]) + ' ' + command[1]);
+            processing_commands.put(command);
 
-        if (stored_command != None):
-            process(stored_command);
-            unstore(stored_command);
 
 def store(command):
     storage_file = open('data', 'a');
     storage_file.write(command + '\n');
     storage_file.close();
 
-def process(command):
-    print('Comando ', command, ' processado com sucesso');
+def process_commands(s):
+    while (True):
+        if(processing_commands.qsize() > 0):
+            command = processing_commands.get();
+            process(command,s);
+
+def process(data,s):
+    (client_addr,command) = data;
+    print('Comando ', data, ' processado com sucesso');
+    command = command.split(' ');
+    operation = command[0];
+    chave_valor = command[1].split(',');
+    chave = chave_valor[0][1:];
+    valor = chave_valor[1][:-1];
+    response = '';
+    if(operation == 'UPDATE'):
+        response = update(chave,valor);
+    if(operation == 'CREATE'):
+        response = create(chave,valor);
+    if(operation == 'READ'):
+        response = read(chave,valor);
+    if(operation == 'DELETE'):
+        response = delete(chave,valor);
+    s.sendto(response.encode(),client_addr)
+
+def update(chave,valor):
+    if(chave in dados):
+        dados[chave] = valor;
+        return 'valor alterado';
+    else:
+        return 'chave nao encontrada';
+
+def create(chave,valor):
+    if(chave not in dados):
+        dados[chave] = valor;
+        return 'valor criado';
+    else:
+        return 'chave ja esta sendo utilizada';
+
+def read(chave,valor):
+    if(chave in dados):
+        return dados[chave];
+    else:
+        return 'chave nao encontrada'
+
+def delete(chave,valor):
+    if(chave in dados):
+        del(dados[chave]);
+        return 'deletado com sucesso'
+    else:
+        return 'chave nao encontrada'
 
 def unstore(command):
     storage_file = open('data', 'r+');
@@ -64,10 +114,12 @@ def read_port():
 
 def main():
     s = config_server();
-    _thread.start_new_thread(store_and_process,());
+    _thread.start_new_thread(store_and_log,());
+    _thread.start_new_thread(process_commands,(s,));
     while(True):
         command, addr = s.recvfrom(12345);
-        received_commands.put(command.decode());
+        print (addr)
+        received_commands.put((addr,command.decode()));
 
 if __name__ == "__main__":
     main();
