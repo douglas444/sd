cc5496eb2601eebc5d9d4bd4a0b80b085662459a
Tiago Faria <tiago_pereira_faria@hotmail.com>
2018-04-14 22:36:07 -0300

    aaa

--- projeto1/client.py
+++ projeto1/client.py
@@ -1,5 +1,7 @@
 import socket;
-
+import _thread;
+import time;
+Comandos = ['CREATE', 'READ', 'UPDATE', 'DELETE'];
 def config_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
     host = socket.gethostname();
@@ -11,12 +13,23 @@ def read_port():
     port = int(config_file.read().split(':')[1]);
     config_file.close();
     return port;
+def console(s,host,port):
+    while True:
+        data = input("entre com um comando: ");
+        comando = data.split(' ');
+        if(comando[0] in Comandos):
+            comando = comando[1].split(',');
+            chave = comando[0][1:];
+            valor = comando[1][:-1];
+            s.sendto(data.encode(), (host, port));
+        else:
+            print("comando invalido");
 
 def main():
     s,host,port = config_server();
+    _thread.start_new_thread(console,(s,host,port));
     while True:
-    	data = input("Entre com um comando: ");
-    	s.sendto(data.encode(), (host, port));
+        time.sleep(10)
 
 if __name__ == "__main__":
     main();
--- projeto1/server.py
+++ projeto1/server.py
@@ -9,68 +9,66 @@ def config_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
     host = socket.gethostname();
     port = read_port();
-    s.bind((host, port));
+    s.bind((host,port));
     return s
 
 def store_and_process():
-	while True:
-		if received_commands.qsize() > 0:
-			command = received_commands.get();
-			store(command);
+    while True:
+        if (received_commands.qsize() > 0):
+            command = received_commands.get();
+            store(command);
+        stored_command = get_stored_command();
 
-		stored_command = get_stored_command();
-
-		if (stored_command != None):
-			process(stored_command);
-			unstore(stored_command);
+        if (stored_command != None):
+            process(stored_command);
+            unstore(stored_command);
 
 def store(command):
-	storage_file = open('data', 'a');
-	storage_file.write(command + '\n');
-	storage_file.close();
+    storage_file = open('data', 'a');
+    storage_file.write(command + '\n');
+    storage_file.close();
 
 def process(command):
-	print('Comando ', command, ' processado com sucesso!');
+    print('Comando ', command, ' processado com sucesso');
 
 def unstore(command):
-	storage_file = open('data', 'r+');
-	file_content = storage_file.read();
-	storage_file.close();
+    storage_file = open('data', 'r+');
+    file_content = storage_file.read();
+    storage_file.close();
 
-	stored_commands = file_content.split('\n');
-	stored_commands.remove(command);
-	file_content = '\n'.join(stored_commands);
+    stored_commands = file_content.split('\n');
+    stored_commands.remove(command);
+    file_content = '\n'.join(stored_commands);
 
-	storage_file = open('data', 'w');
-	storage_file.write(file_content);
-	storage_file.close();
+    storage_file = open('data','r+');
+    storage_file.write(file_content);
+    storage_file.close();
 
 def get_stored_command():
-	storage_file = open('data', 'r+');
-	file_content = storage_file.read();
-	storage_file.close();
+    storage_file = open('data','r+');
+    file_content = storage_file.read();
+    storage_file.close();
 
-	stored_commands = file_content.split('\n');
+    stored_commands = file_content.split('\n');
 
-	if file_content and len(stored_commands) > 0:
-		return stored_commands[0];
-	else:
-		return None;
+    if(file_content and len(stored_commands) > 0):
+        return stored_commands[0];
+    else:
+        return None;
 
 def read_port():
     config_file = open('config-servidor.txt','r');
     port = int(config_file.read().split(':')[1]);
-    config_file.close();
+    config_file.close;
     return port;
-
 
 def main():
     s = config_server();
-
-    #_thread.start_new_thread(store_and_process, ());
-    #while True:
-    	#command, addr = s.recvfrom(12345);
-    	#received_commands.put(command.decode());
+    _thread.start_new_thread(store_and_process,());
+    while(True):
+        command, addr = s.recvfrom(12345);
+        x = 2;
+        received_commands.put(command.decode());
 
 if __name__ == "__main__":
     main();
